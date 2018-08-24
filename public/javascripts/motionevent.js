ffffaa3900cb0a8e75e3a881f2752c04f399756c
Tener Hades <tener.hades@gmail.com>
2018-08-24 11:01:17 +0100

    wip motion averaging

--- public/javascripts/motionevent.js
+++ public/javascripts/motionevent.js
@@ -5,10 +5,27 @@ window.addEventListener('load', (ev) => {
     }
 });
 
+
+/**
+ * Display the averages of the last few event properties
+ * @param {Event} DeviceMotionEvent event
+ */
 function handleMotion(ev) {
     const aX = ev.accelerationIncludingGravity.x*1;
     const aY = ev.accelerationIncludingGravity.y*1;
     const aZ = ev.accelerationIncludingGravity.z*1;
+
+    if (!window.accelHistory) {
+        window.accelHistory = {
+            x: [aX],
+            y: [aY],
+            z: [aZ],
+        };
+    } else {
+        window.accelHistory.x.push(aX);
+        window.accelHistory.y.push(aX);
+        window.accelHistory.z.push(aZ);
+    }
 
     document.querySelector('#x').innerHTML = aX.toFixed(2);
     document.querySelector('#y').innerHTML = aY.toFixed(2);
