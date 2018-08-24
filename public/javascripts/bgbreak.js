c9c1ef7364feb986333dc82fc568529372b05bd6
Tener Hades <tener.hades@gmail.com>
2018-08-24 10:42:07 +0100

    removed bgbreak.js

--- public/javascripts/bgbreak.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', (ev) => {
-    // Array.from(document.querySelectorAll('.bg-break')).forEach((el) => {
-    //     el.style.minHeight = String(window.innerHeight || document.documentElement.clientHeight) + 'px';
-    // });
-});
